01b4899fbc6ef32281ab89b62cca9be4b900dddb
Almog <48240399+OlympicAngel@users.noreply.github.com>
2022-11-22 01:17:03 +0200

    Update MouseEvents.js

--- MouseEvents.js
+++ MouseEvents.js
@@ -32,13 +32,13 @@ function HandleBinaryOutput(data) {
 }
 
 const events = {
-    move: [],
-    left_down: [],
-    left_up: [],
-    right_down: [],
-    right_up: [],
-    wheel: [],
-    any: []
+    move: [], //array of functions that should run when "move" triggers
+    left_down: [], //array of functions that should run when "left_down" triggers
+    left_up: [], //array of functions that should run when "left_up" triggers
+    right_down: [], //array of functions that should run when "right_down" triggers
+    right_up: [], //array of functions that should run when "right_up" triggers 
+    wheel: [], //array of functions that should run when "wheel" triggers
+    any: [] //array of functions that should run when-ever one of the above gets events triggered
 }
 
 module.exports = {
@@ -76,7 +76,7 @@ module.exports = {
 
         let func_index = events[event_name].indexOf(cb);
         if (func_index == -1)
-            return new Error("This function is not registered in that event..");
+            return new Error("This function is not registered in that event.."); // note it wont throw error but returns an error as this is not fatal.
 
         events[event_name].splice(func_index, 1);
     }
