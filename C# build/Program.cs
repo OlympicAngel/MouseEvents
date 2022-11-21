90f1519a8d75ce1ab9e4a6a6f1fd32ee738f7651
Almog <48240399+OlympicAngel@users.noreply.github.com>
2022-11-22 00:38:25 +0200

    Updated C# project
    was not defined properly - fixed that

--- C# build/Program.cs
+++ C# build/Program.cs
@@ -1,55 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
 using System.Drawing;
 using System.Reflection.Metadata;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
-using MiceDetect;
 
-internal class Program
+namespace MiceDetect
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-        MiceDetector.Start();
-        MiceDetector.MouseAction += new EventHandler(Event);
-
-
-        handler = new ConsoleEventDelegate(ConsoleEventCallback);
-        SetConsoleCtrlHandler(handler, true);
-
-
-        Application.Run(new ApplicationContext());
-    }
-
-    #region clear event
-    static bool ConsoleEventCallback(int eventType)
-    {
-        if (eventType == 2)
+        private static void Main(string[] args)
         {
-            MiceDetector.stop();
+            MiceDetector.Start();
+            MiceDetector.MouseAction += new EventHandler(Event);
+
+
+            handler = new ConsoleEventDelegate(ConsoleEventCallback);
+            SetConsoleCtrlHandler(handler, true);
+
+            Application.Run(new ApplicationContext());
         }
-        return false;
+
+        #region clear event
+        static bool ConsoleEventCallback(int eventType)
+        {
+            if (eventType == 2)
+            {
+                MiceDetector.stop();
+            }
+            return false;
+        }
+        static ConsoleEventDelegate handler;   // Keeps it from getting garbage collected
+                                               // Pinvoke
+        private delegate bool ConsoleEventDelegate(int eventType);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool SetConsoleCtrlHandler(ConsoleEventDelegate callback, bool add);
+        #endregion
+
+
+        #region handle event & output
+        private static void Event(object sender, EventArgs e)
+        {
+            var pt = new Point();
+            GetCursorPos(ref pt);
+
+            string json = "{'action':'" + e + "','cords':[" + pt.X + "," + pt.Y + "]}";
+
+            Console.WriteLine(json);
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(ref Point lpPoint);
+        #endregion
     }
-    static ConsoleEventDelegate handler;   // Keeps it from getting garbage collected
-    // Pinvoke
-    private delegate bool ConsoleEventDelegate(int eventType);
-    [DllImport("kernel32.dll", SetLastError = true)]
-    private static extern bool SetConsoleCtrlHandler(ConsoleEventDelegate callback, bool add);
-    #endregion
-
-
-    #region handle event & output
-    private static void Event(object sender, EventArgs e)
-    {
-        var pt = new Point();
-        GetCursorPos(ref pt);
-
-        string json = "{'action':'" + e + "','cords':[" + pt.X + "," + pt.Y + "]}";
-
-        Console.WriteLine(json);
-    }
-
-    [DllImport("user32.dll")]
-    static extern bool GetCursorPos(ref Point lpPoint);
-    #endregion
-
 }
