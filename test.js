7ff2843f89703dcfcd4724ea8f86096623d0b74e
Almog <48240399+OlympicAngel@users.noreply.github.com>
2022-11-22 00:58:15 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,36 @@
     var MouseEvents = require('/mouse-events.js');
 
-    MouseEvents.on("move", console.log)
+    MouseEvents.on("move", (e)=>{
+        console.log("move event triggred with:")
+        cnsole.log(e)
+    })
+
+    MouseEvents.on("wheel", (e)=>{
+        console.log("wheel event triggred with:")
+        cnsole.log(e)
+    })
+
+    MouseEvents.on("left_down", (e)=>{
+        console.log("left_down event triggred with:")
+        cnsole.log(e)
+    })
+
+    MouseEvents.on("left_up", (e)=>{
+        console.log("left_up event triggred with:")
+        cnsole.log(e)
+    })
+
+    MouseEvents.on("right_down", (e)=>{
+        console.log("right_down event triggred with:")
+        cnsole.log(e)
+    })
+
+    MouseEvents.on("right_up", (e)=>{
+        console.log("right_up event triggred with:")
+        cnsole.log(e)
+    })
+
+    MouseEvents.on("any", (e)=>{
+        console.log("some event triggred with:")
+        cnsole.log(e)
+    })
